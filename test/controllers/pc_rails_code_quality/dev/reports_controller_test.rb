171d3558101c43b3cca91ff840f448d73881328a
Slopy <mic.mar@live.com>
2016-12-24 12:53:39 +0100

    Add controller and view test for rubocop report

--- test/controllers/pc_rails_code_quality/dev/reports_controller_test.rb
+++ test/controllers/pc_rails_code_quality/dev/reports_controller_test.rb
@@ -9,6 +9,12 @@ module PcRailsCodeQuality
       assert_response :success
     end
 
+    test "#rubocop iframe" do
+      get dev_rubocop_html_report_path
+      assert_select 'iframe#rubocop-html-report'
+      assert_select 'iframe#rubocop-html-report', href: /rubocop/
+    end
+
     test "#run_rubocop" do
       get dev_run_rubocop_html_report_path
       assert_redirected_to dev_rubocop_html_report_path
