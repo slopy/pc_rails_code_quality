1f2b8ce20086031c5d9646e0f128eb6f170f6b8c
Slopy <mic.mar@live.com>
2016-12-24 12:54:09 +0100

    Use pry in tests
    - ignore publi/ engine folder
    ## To make a commit, type your commit message and press SUPER-ENTER. To cancel
    ## the commit, close the window. To sign off on the commit, press SUPER-S.
    ##
    ## You may also reference or close a GitHub issue with this commit.  To do so,
    ## type `#` followed by the `tab` key.  You will be shown a list of issues
    ## related to the current repo.  You may also type `owner/repo#` plus the `tab`
    ## key to reference an issue in a different GitHub repo.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -18,3 +18,6 @@ if ActiveSupport::TestCase.respond_to?(:fixture_path=)
   ActiveSupport::TestCase.file_fixture_path = ActiveSupport::TestCase.fixture_path + "/files"
   ActiveSupport::TestCase.fixtures :all
 end
+
+require 'pry'
+
