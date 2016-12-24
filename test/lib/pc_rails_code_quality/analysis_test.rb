0f08bf6108073afa3916feacf0a636664554230d
Slopy <mic.mar@live.com>
2016-12-24 12:53:50 +0100

    Add analys lib test

---
+++ test/lib/pc_rails_code_quality/analysis_test.rb
@@ -0,0 +1,9 @@
+require 'test_helper'
+class PcRailsCodeQuality::Analysis::Test < ActiveSupport::TestCase
+
+  test "#run_rubocop_html_report generate a file" do
+    PcRailsCodeQuality::Analysis.run_rubocop_html_report
+    assert File.open('public/reports/rubocop.html')
+  end
+
+end
