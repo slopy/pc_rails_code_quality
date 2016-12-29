f844ad9b9f4f95dfc3bb05f7e50491bddc8ec799
Slopy <mic.mar@live.com>
2016-12-29 12:03:03 +0100

    Add routes for rubicritic reports

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ PcRailsCodeQuality::Engine.routes.draw do
   namespace :dev do
     get 'html_reports/rubocop', as: :rubocop_html_report
     get 'html_reports/run_rubocop', as: :run_rubocop_html_report
+    get 'html_reports/rubycritic', as: :rubycritic_html_report
+    get 'html_reports/run_rubycritic', as: :run_rubycritic_html_report
   end
 end
