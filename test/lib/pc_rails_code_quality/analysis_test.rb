91c32cc874a73d65780a9405d14aad350b25755c
Slopy <mic.mar@live.com>
2016-12-29 12:04:40 +0100

    Add test coverage

--- test/lib/pc_rails_code_quality/analysis_test.rb
+++ test/lib/pc_rails_code_quality/analysis_test.rb
@@ -5,4 +5,10 @@ class PcRailsCodeQuality::Analysis::Test < ActiveSupport::TestCase
     PcRailsCodeQuality::Analysis.run_rubocop_html_report
     assert File.open('public/reports/rubocop.html')
   end
+  
+  test '#run_run_rubycritic_html_report_html_report generate a file' do
+    PcRailsCodeQuality::Analysis.run_rubycritic_html_report
+    assert File.open('public/reports/rubocop.html')
+  end
+
 end
