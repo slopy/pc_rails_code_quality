e0fc6c510847ea328ea7dceafd33b7db45bf62e6
Slopy <mic.mar@live.com>
2017-01-04 09:46:30 +0100

    Cover rake tasks with tests

---
+++ test/lib/tasks/code_quality_html_reports_test.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+require 'test_helper'
+class CodeQualityHtmlReportsRakeTaskTest < ActiveSupport::TestCase
+
+  setup do 
+    require 'rake'
+    Rake::Task.define_task :environment
+    Rails.application.load_tasks
+  end
+
+  teardown do 
+    Rake::Task.clear
+    FileUtils.rm_rf(Rails.root + 'public/reports')
+  end
+
+  test 'rake pc_reports:html is generating reports' do
+    Rake::Task['pc_reports:html'].reenable
+    Rake::Task['pc_reports:html'].invoke
+    assert File.exist?(Rails.root + 'public/reports/rubocop.html')
+    assert File.exist?(Rails.root + 'public/reports/ruby_critic/overview.html')
+    assert File.exist?(Rails.root + 'public/reports/tests/index.html')
+    assert File.exist?(Rails.root + 'public/reports/rails_best_practices.html')
+    assert File.exist?(Rails.root + 'public/reports/brakeman.html')
+    assert File.exist?(Rails.root + 'public/reports/simplecov/index.html')
+  end
+end
